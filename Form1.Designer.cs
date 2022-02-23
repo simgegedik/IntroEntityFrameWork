d7f6ccfb8bf2451f217c303f11be7bc81f9a16e0
Simge <Simge@DESKTOP-D1P3DBS>
2022-02-23 22:42:23 +0300

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,77 @@
+﻿
+namespace IntroEntityFrameWork
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dvgMusteri = new System.Windows.Forms.DataGridView();
+            this.btnGetir = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dvgMusteri)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dvgMusteri
+            // 
+            this.dvgMusteri.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dvgMusteri.Location = new System.Drawing.Point(22, 149);
+            this.dvgMusteri.Name = "dvgMusteri";
+            this.dvgMusteri.RowHeadersWidth = 62;
+            this.dvgMusteri.RowTemplate.Height = 28;
+            this.dvgMusteri.Size = new System.Drawing.Size(750, 289);
+            this.dvgMusteri.TabIndex = 0;
+            // 
+            // btnGetir
+            // 
+            this.btnGetir.Location = new System.Drawing.Point(22, 52);
+            this.btnGetir.Name = "btnGetir";
+            this.btnGetir.Size = new System.Drawing.Size(121, 49);
+            this.btnGetir.TabIndex = 1;
+            this.btnGetir.Text = "Getir";
+            this.btnGetir.UseVisualStyleBackColor = true;
+            this.btnGetir.Click += new System.EventHandler(this.btnGetir_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnGetir);
+            this.Controls.Add(this.dvgMusteri);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dvgMusteri)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dvgMusteri;
+        private System.Windows.Forms.Button btnGetir;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace IntroEntityFrameWork
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnGetir_Click(object sender, EventArgs e)
+        {
+            #region Eski Yöntemler
+            //SqlConnection cnn = new SqlConnection("data source =.; initial catalog = Northwind; integrated security = True; MultipleActiveResultSets = True; App = EntityFramework");
+
+            //SqlCommand cmd = new SqlCommand("Select * From Customers", cnn);
+            //if (cnn.State==ConnectionState.Closed)
+            //{
+            //    cnn.Open();
+            //}
+            //SqlDataReader dr   =  cmd.ExecuteReader();
+            //DataTable dt = new DataTable();
+            //dt.Load(dr);
+            //cnn.Close();
+            //dvgMusteri.DataSource = dt;
+
+            //SqlDataAdapter dap = new SqlDataAdapter("Select * From Products", "data source =.; initial catalog = Northwind; integrated security = True; MultipleActiveResultSets = True; App = EntityFramework");
+            //DataTable dt = new DataTable();
+            //dap.Fill(dt);
+            //dvgMusteri.DataSource = dt;
+
+            //Entity Frameworkle de aşağıdaki şekilde getirilmiş oldu.
+
+            NorthwindEntities db = new NorthwindEntities();
+            dvgMusteri.DataSource = db.Customers.ToList();
+
+
+            #endregion
+        }
+    }
+}
